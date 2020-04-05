c183c25dd0201806bb625d3425ebb1c25abf4627
U. Shafiq <ushafiq@me.com>
2020-04-05 04:33:28 +0200

    Update trailer-download.sh

--- trailer-download.sh
+++ trailer-download.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ############################################################
-# MOVIE_FOLDER is the root directory
+# MOVIES_DIR is the root directory
 # it contains sub directories with year names
 # each year directory contains movie directory
 # Movie Format: Movies/{y}/{n}/{n} {y} {hd} {hdr}
@@ -10,10 +10,9 @@
 # ----------------------------------------------------------
 
 # Set movies root directory
-MOVIE_DIRECTORY="/volume1/video/Movies/"
+MOVIES_DIR="/volume1/video/Movies/"
 
-
-cd $MOVIE_DIRECTORY
+cd $MOVIES_DIR
 directories=(*/)
 
 for dir in "${directories[@]}"
@@ -25,21 +24,23 @@ do
 	do
 		cd "$subDir"
 		echo `pwd`
-
+		
 		subDirName=${subDir%*/}
 		echo $subDirName
-
 		# Download trailer
 		if find . -iname '*trailer*' -printf 1 -quit | grep -q 1
 		then
 		    TRAILER_FILE_NAME=`find $subDirName -type f -iname "*trailer*"`
-		    echo Trailer found: ${TRAILER_FILE_NAME}
+		    echo "Trailer found: ${TRAILER_FILE_NAME}"
 		else
 		    echo Trailer not found, downloading...
 		    MOVIE_NAME=`find . -type f -regex '.*\.\(mp4\|mkv\|avi\)'`
 		    MOVIE_NAME="${MOVIE_NAME##*/}"
+		    echo "$MOVIE_NAME"
+		    echo "${MOVIE_NAME%.*}"
 		    /usr/local/bin/youtube-dl "ytsearch1: ${MOVIE_NAME%.*} trailer"
 		    NEW_TRAILER_FILE=`find . -type f -iname "*trailer*"`
+		    echo ${NEW_TRAILER_FILE##*/}
 		    mv "${NEW_TRAILER_FILE##*/}" "${MOVIE_NAME%.*}-trailer.mkv"
 		fi
 		
